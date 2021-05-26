6a5086cf25c544c3ad031f0627faac7a0f3c51de
Rajat Ambastha <rajatambastha0034@gmail.com>
2021-05-26 23:22:10 +0530

    Add remote url in bash and test

--- src/main/java/com/rj/springboot/project/cruddemo/entity/Employee.java
+++ src/main/java/com/rj/springboot/project/cruddemo/entity/Employee.java
@@ -72,7 +72,7 @@ public class Employee {
 
 	@Override
 	public String toString() {
-		return "Employee Info--> [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
+		return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
 	}
 	
 }
