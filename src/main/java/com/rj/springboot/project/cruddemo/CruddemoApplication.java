92113fa6281984ad53a343e6748e9709f84e2e05
Rajat Ambastha <rajatambastha0034@gmail.com>
2021-07-28 15:32:01 +0530

    Testing a conflict

--- src/main/java/com/rj/springboot/project/cruddemo/CruddemoApplication.java
+++ src/main/java/com/rj/springboot/project/cruddemo/CruddemoApplication.java
@@ -8,6 +8,8 @@ public class CruddemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CruddemoApplication.class, args);
+		System.out.println("This might create a conflict");
+		System.out.println("And this might create a conflict");
 	}
 
 }
